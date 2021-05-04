48253a533f365070f1bbc831a2ffb95968ac8deb
viniciusfcar <viniciuscarneiro007@gmail.com>
2021-05-03 21:34:48 -0300

    tela de detalhes

--- src/pages/CadastroProducer/CadastroProducer.js
+++ src/pages/CadastroProducer/CadastroProducer.js
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import Navbar from '../../components/NavBar/Navbar.js';
+import Modal from 'react-modal';
 import { useParams } from "react-router-dom";
 import './CadastroProducer.css';
-import { activities, periods } from '../../enums'
-import Select from 'react-select'
+import { Alert } from 'react-bootstrap';
 
-<<<<<<< HEAD
 function CadastroProducer() {
 
 
@@ -23,104 +22,130 @@ function CadastroProducer() {
     //variáveis do Modal
     const [modal, setModal] = useState(false);
     const [msgModal, setMsgModal] = useState('');
-=======
-import api from '../../services/api'
 
-function CadastroProducer() {
->>>>>>> dd579d25ba9fd01e10a10a2138b53dbdbaaefdbb
-
-    // Producer
+    //variáveis do produtor
     let { id } = useParams();
     const [name, setName] = useState("");
     const [nickname, setNickname] = useState("");
     const [phone, setPhone] = useState("");
     const [email, setEmail] = useState("");
     const [cpf, setCpf] = useState("");
-    const [status, setStatus] = useState("");
-    const [role, setRole] = useState("");
+    const [status, setStatus] = useState(true);
+    const [role, setRole] = useState(1);
     const [birthDate, setBirthDate] = useState("");
-    const [activity, setActivity] = useState('');
-    const [period, setPeriod] = useState('');
+    const [address, setAddress] = useState({});
+    const [farmingActivity, setFarmingActivity] = useState({});
     const [products, setProducts] = useState([]);
-
-    // Produtos
-    const [productList, setProductList] = useState([]);
     
+    //variáveis do Endereço
+    const [uf, setUf]  = useState();
+    const [city, setCity]  = useState();
+    const [zipCode, setZipCode]  = useState();
+    const [district, setDistrict]  = useState();
+    const [street, setStrret]  = useState();
+    const [houseNamber, setHousenamber]  = useState();
+    const [reference, setReference]  = useState();
+    
+    //variáveis da atividade
+    const [activityName, setActivityName] = useState('');
+    const [period, setPeriod] = useState('');
+    const [averageCash, setCash] = useState('');
 
-    // Address
-    const [zipCode, setZipCode] = useState('')
-    const [city, setCity] = useState('')
-    const [district, setDistrict] = useState('')
-    const [street, setStreet] = useState('')
-    const [uf, setUf] = useState('')
-    const [houseNumber, setHouseNumber] = useState('')
-    const [reference, setReference] = useState('')
-    const [averageCash, setAverageCash] = useState();
+    //variáveis do produto
+    const [productList, setProductList] = useState([]);
 
+    
     //recupera dados do produtor na API
     const getProducer = async (id) => {
+        const request = await fetch('https://apiproducers.serviceapp.net.br/api/producers/'+id)
+        const response = await request.json();  
+        setVars(response);
+    }
 
-        const request = await api.getAllProducers(id)
-        const response = await request.json();
-        setName(response.name);
-        setNickname(response.nickname);
-        setPhone(response.phone);
-        setEmail(response.email);
-        setCpf(response.cpf);
-        setStatus(response.status);
-        setRole(response.role);
-        setBirthDate(new Date());
-
-        setZipCode(response.address.zipCode)
-        setCity(response.address.city)
-        setStreet(response.address.street)
-        setDistrict(response.address.district)
-        setUf(response.address.uf)
-        setHouseNumber(response.address.houseNumber)
-        setReference(response.address.reference)
-
-        setProducts([...response.products] || []);
-        setActivity(response.farmingActivity.activityName);
-        setPeriod(response.farmingActivity.period);
-        setAverageCash(response.farmingActivity.averageCash)
-
+    const setVars = async (producer) => {
+        id = producer.id;
+        setName(producer.name);
+        setNickname(producer.nickname);
+        setPhone(producer.phone);
+        setEmail(producer.email);
+        setCpf(producer.cpf);
+        setStatus(producer.status);
+        setRole(producer.role);
+        setBirthDate(producer.birthDate);
+        setAddress(producer.address);
+        setProducts(producer.products);
+        await setFarmingActivity(producer.farmingActivity);
+        
     }
 
     //recupera lista de produtos na API
     const getProducts = async () => {
-        const request = await api.getAllProducts()
+        const request = await fetch('https://apiproducers.serviceapp.net.br/api/products', {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        }); 
         const response = await request.json();
         setProductList(response);
     }
-
+    
+    //carrega dados da API
     useEffect(() => {
-        if (id > 0) {
+        if(id > 0) {
+            //dados do produtor
             getProducer(id);
-        }
-        getProducts()
+        } 
+        //lista de produtos
+        getProducts();
     }, [])
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-
-        await api.updateProducer(
-            id, name, nickname, birthDate, phone, cpf, email, houseNumber, reference,
-            averageCash, zipCode, city, district, uf, street, activity, resultList, period
-        )
+    //fecha caixa de diálogo modal
+    const closeModal = () => {
+        setModal(false);
     }
 
-    const productsList = () => {
-        const newArray = []
-        for (let i of products) {
-            const keys = Object.keys(i)
-            for (let key of keys) {
-                if (key === 'value') {
-                    const obj = { value: i[key] }
-                    newArray.push(obj)
-                }
+    //valida formulário antes do envio
+    const validaForm = () => {
+        if(
+            name == '' || nickname == '' || phone == '' || 
+            email == '' ||  cpf == '' || birthDate == '' 
+        ) {
+            setMsgModal('Preenchha os campos obrigatórios marcado com *');
+            setModal(true);
+        } else {
+            onCadastroProducer();
+        }   
+    }
+
+    //salva dados para a API
+    const onCadastroProducer = async () => {
+        const body = {
+            name: name,
+            nickname, nickname,
+            phone: phone, 
+            email: email,
+            cpf: cpf, 
+            status: status, 
+            role: role,
+            birthDate: birthDate,
+            address: {
+                zipCode : address.zipCode,
+                city : address.city,
+                uf : address.uf,
+                district : address.district,
+                street : address.street,
+                houseNambe : address.houseNamber,
+                reference : address.reference
+            },
+            farmingActivity: {
+                activityName : farmingActivity.activityName,
+                period : farmingActivity.period,
+                averageCash : farmingActivity.averageCash
+            },
+            products: products,
+            manager : {
+                id : 1
             }
         }
-<<<<<<< HEAD
         
         let url = 'https://apiproducers.serviceapp.net.br/api/producers';
         let method = 'POST';
@@ -178,95 +203,77 @@ function CadastroProducer() {
             </Modal>
             <nav class="navbar" style={{marginTop: 10, backgroundColor: 'lightgray'}}>
                 <div class="container-fluid" style={{alignItems: 'center', justifyContent: 'space-around'}}>
-=======
-        return newArray
-    }
-
-    const resultList = productsList()
-
-    return (
-        <>
-            <Navbar />
-            <nav class="navbar" style={{ marginTop: 10, backgroundColor: 'lightgray' }}>
-                <div class="container-fluid" style={{ alignItems: 'center', justifyContent: 'space-around' }}>
->>>>>>> dd579d25ba9fd01e10a10a2138b53dbdbaaefdbb
                     <h3>Cadastro Produtor</h3>
                 </div>
             </nav>
             <div className="container">
-                <form class="row g-3" onSubmit={handleSubmit}>
-                <div class="col-md-2">
-                        <label for="id" class="form-label">Apelido</label>
-                        <span type="text" class="form-control" id="id">{id}</span>
+                <form class="row g-3">
+                    <div class="col-md-2">
+                        <label for="nickname" class="form-label">ID#</label>
+                        <span class="form-control">{id}</span>
                     </div>
-                    <div class="col-10">
-                        <label for="name" class="form-label">Nome</label>
-                        <input type="text" class="form-control" id="kame" placeholder="Ex: João de Oliveira" onChange={function (event) { setName(event.target.value) }} value={name} />
+                    <div class="col-md-5">
+                        <label for="nickname" class="form-label">Nick Name*</label>
+                        <input type="email" class="form-control" id="nickname" onChange={function (event) {setNickname(event.target.value)}} value={nickname}/>
                     </div>
-                    <div class="col-md-6">
-                        <label for="nickname" class="form-label">Apelido</label>
-                        <input type="text" class="form-control" id="nickname" onChange={function (event) { setNickname(event.target.value) }} value={nickname} />
+                    <div class="col-md-5">
+                        <label for="inputPassword4" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="password"/>
                     </div>
-                    <div class="col-6">
-                        <label for="email" class="form-label">E-mail</label>
-                        <input type="email" class="form-control" id="email" placeholder="Ex: joao@gmail.com" onChange={function (event) { setEmail(event.target.value) }} value={email} />
+                    <div class="col-12">
+                        <label for="password" class="form-label">Nome*</label>
+                        <input type="text" class="form-control" id="kame" placeholder="Ex: João de Oliveira" onChange={function (event) {setName(event.target.value)}} value={name}/>
                     </div>
-                    <div class="col-md-4">
-                        <label for="phone" class="form-label">Telefone</label>
-                        <input type="text" class="form-control" id="phone" onChange={function (event) { setPhone(event.target.value) }} value={phone} />
-                    </div>
-                    <div class="col-md-4">
-                        <label for="cpf" class="form-label">CPF</label>
-                        <input type="text" class="form-control" id="cpf" onChange={function (event) { setCpf(event.target.value) }} value={cpf} />
+                    <div class="col-12">
+                        <label for="email" class="form-label">E-mail*</label>
+                        <input type="email" class="form-control" id="email" placeholder="Ex: joao@gmail.com" onChange={function (event) {setEmail(event.target.value)}} value={email}/>
                     </div>
                     <div class="col-md-4">
-                        <label for="birthDate" class="form-label">Data de Nascimento</label>
-                        <input type="date" class="form-control" id="birthDate" onChange={function (event) { setBirthDate(event.target.value) }} value={birthDate} />
+                        <label for="phone" class="form-label">Telefone*</label>
+                        <input type="text" class="form-control" id="phone" onChange={function (event) {setPhone(event.target.value)}} value={phone}/>
+                    </div>
+                    <div class="col-md-4">
+                        <label for="cpf" class="form-label">CPF*</label>
+                        <input type="text" class="form-control" id="cpf" onChange={function (event) {setCpf(event.target.value)}} value={cpf}/>
+                    </div>
+                    <div class="col-md-4">
+                        <label for="birthDate" class="form-label">Data de Nascimento*</label>
+                        <input type="date" class="form-control" id="birthDate" onChange={function (event) {setBirthDate(event.target.value)}} value={birthDate}/>{birthDate}
                     </div>
                     <div class="row g-2">
                         <div class="col-md-3">
                             <label for="zipCode" class="form-label">CEP</label>
-                            <input type="text" class="form-control" id="zipCode" onChange={e => setZipCode(e.target.value)} value={zipCode} />
+                            <input type="text" class="form-control" id="zipCode" onChange={async (event) => {var a = address; a.zipCode = event.target.value; await setAddress(a)}} value={address.zipCode}/>
                         </div>
                         <div class="col-md-7">
                             <label for="street" class="form-label">Logradouro</label>
-                            <input type="text" class="form-control" id="street" onChange={e => setStreet(e.target.value)} value={street} />
+                            <input type="text" class="form-control" id="street" value={address.street}/>
                         </div>
                         <div class="col-md-2">
                             <label for="houseNumber" class="form-label">Número</label>
-                            <input type="text" class="form-control" id="houseNumber" onChange={e => setHouseNumber(e.target.value)} value={houseNumber} />
+                            <input type="text" class="form-control" id="houseNumber" value={address.houseNumber}/>
                         </div>
                         <div class="col-md-6">
                             <label for="reference" class="form-label">Complemento</label>
-                            <input type="text" class="form-control" id="reference" onChange={e => setReference(e.target.value)} value={reference} />
+                            <input type="text" class="form-control" id="reference" value={address.reference}/>
                         </div>
                         <div class="col-md-6">
                             <label for="district" class="form-label">Bairro</label>
-                            <input type="text" class="form-control" id="district" onChange={e => setDistrict(e.target.value)} value={district} />
+                            <input type="text" class="form-control" id="district" value={address.district}/>
                         </div>
-                        <div class="col-md-8">
+                        <div class="col-md-6">
                             <label for="city" class="form-label">Cidade</label>
-                            <input type="text" class="form-control" id="city" onChange={e => setCity(e.target.value)} value={city} />
+                            <input type="text" class="form-control" id="city" value={address.city}/>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-6">
                             <label for="uf" class="form-label">Estado</label>
                             <select id="uf" class="form-select">
-                                <option value="" selected={uf == "" ? true : false}>Selecione</option>
-                                <option value="RN" selected={uf == "RN" ? true : false}>RN</option>
+                            <option value="" selected={address.uf=="" ? true : false}>Selecione</option>
+                            <option value="RN" selected={address.uf=="RN" ? true : false}>RN</option>
                             </select>
                         </div>
                     </div>
-                    <div class="col-4">
-                        <label for="inputState" class="form-label">Atividade Agrícola</label>
-                        <select id="inputState" class="form-select" onChange={e => setActivity(e.target.value)}>
-                            <option selected>Selecione</option>
-                            {activities.map((i) => (
-                                <option key={i.value} value={i.value}>{i.label}</option>
-                            ))}
-                        </select>
-                    </div>
 
-<<<<<<< HEAD
                     <div class="col-12 card">
                         <div class="col-12 card">
                             <label for="productList" class="form-label">Atividade Agrícola</label>
@@ -332,33 +339,9 @@ function CadastroProducer() {
                     <div class="col-6">
                         <button type="submit" class="btn btn-primary" onClick={validaForm}>Salvar</button>
                     </div>                    
-=======
-                    <div class="col-4">
-                        <label for="inputState" class="form-label">Périodo da Renda</label>
-                        <select id="inputState" class="form-select" onChange={e => setPeriod(e.target.value)}>
-                            <option selected>Selecione</option>
-                            {periods.map((i) => (
-                                <option key={i.value} value={i.label}>{i.label}</option>
-                            ))}
-                        </select>
-                    </div>
-
-                    <label for="inputState" class="form-label">Selecione os Produtos</label>
-                    <Select
-                        options={productList}
-                        value={products}
-                        isMulti
-                        onChange={(item) => setProducts(item)}
-                    />
-
-                    <div class="col-12">
-                        <button type="submit" class="btn btn-primary">Cadastrar</button>
-                    </div>
->>>>>>> dd579d25ba9fd01e10a10a2138b53dbdbaaefdbb
                 </form>
             </div>
         </>
     );
 }
-
 export default CadastroProducer;
