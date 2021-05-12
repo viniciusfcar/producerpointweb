3c9808b1d069947a3469ccbf26848d0b438ee12d
moizez <moizezhenrique@gmail.com>
2021-05-12 11:16:01 -0300

    adicionei o nome do usuario logado na home e no navbar

--- src/components/NavBar/Navbar.js
+++ src/components/NavBar/Navbar.js
@@ -1,53 +1,58 @@
+import React from 'react'
 import logo from '../../imagens/logo.png';
 import './Navbar.css';
 
 import { doLogout } from '../../services/auth'
+import { AuthContext } from '../Context/AuthContext';
 
 const Navbar = () => {
+
+    const { user } = React.useContext(AuthContext)
 
     const handleLogout = () => {
         doLogout()
         window.location.href = '/'
     }
 
-    return(
+    return (
         <>
             <nav class="navbar navbar-expand-lg navbar-dark bg-success">
-                <div class="container-fluid">
-                    <img src={logo} style={{height : '48px'}} className="logo" alt="logo"/>
-                    <a class="navbar-brand" href="/home">ProducerPoint</a>
+                <div className="container-fluid">
+                    <img src={logo} style={{ height: '48px' }} className="logo" alt="logo" />
+                    <a class="navbar-brand" href="/home">Producer Point</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                        <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <a class="nav-link" aria-current="page" href="/Home">Home</a>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Cadastro
+                        <div class="navbar-nav">
+                            <a class="nav-link" aria-current="page" href="/Home">Home</a>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Cadastro
                             </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <li><a class="dropdown-item" href="/cadastro-activity">Atividade</a></li>
-                                <li><a class="dropdown-item" href="/cadastro-producer">Produtor</a></li>
-                                <li><a class="dropdown-item" href="/cadastro-product">Produto</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle"  href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Listar
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><a class="dropdown-item" href="/cadastro-activity">Atividade</a></li>
+                                    <li><a class="dropdown-item" href="/cadastro-producer">Produtor</a></li>
+                                    <li><a class="dropdown-item" href="/cadastro-product">Produto</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Listar
                             </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <li><a class="dropdown-item" href="/lista-activities">Atividade</a></li>
-                                <li><a class="dropdown-item" href="/lista-producers">Produtores</a></li>
-                                <li><a class="dropdown-item" href="/lista-products">Produtos</a></li>
-                            </ul>
-                        </li>
-                        <a onClick={handleLogout} class="nav-link" href="#" tabindex="-1" aria-disabled="true">Sair</a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><a class="dropdown-item" href="/lista-activities">Atividade</a></li>
+                                    <li><a class="dropdown-item" href="/lista-producers">Produtores</a></li>
+                                    <li><a class="dropdown-item" href="/lista-products">Produtos</a></li>
+                                </ul>
+                            </li>
+                            <a onClick={handleLogout} class="nav-link" href="#" tabindex="-1" aria-disabled="true">Sair</a>
+                        </div>
                     </div>
-                    </div>
+                    <h4>Olá, {user.name}</h4>
                 </div>
             </nav>
-        </> 
+        </>
     );
 }
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../components/Context/AuthContext.js';
 import Navbar from '../../components/NavBar/Navbar.js';
 
 import './Home.css';
 
 function Home() {
 
-    return(
-        <> 
-            <Navbar/> 
-            <nav class="navbar" style={{marginTop: 10, backgroundColor: 'lightgray'}}>
-                <div class="container-fluid" style={{alignItems: 'center', justifyContent: 'space-around'}}>
-                    <h3>Seja bem-bindo ao ProducerPoint</h3>
+    const { user } = useContext(AuthContext)
+
+    return (
+        <>
+            <Navbar />
+            <nav class="navbar" style={{ marginTop: 10, backgroundColor: 'lightgray' }}>
+                <div className="container" style={{ alignItems: 'center', justifyContent: 'space-around' }}>
+                    <h3>Seja bem-vindo {user.name}</h3>
                 </div>
             </nav>
         </>
